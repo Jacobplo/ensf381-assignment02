31ad418bd78216f31e6562e68d25fa3192d9ef44
Jacob Plourde <jacob_plourde@hotmail.com>
2026-03-09 22:40:14 -0600

    Finish customize_order

--- customize_order.js
+++ customize_order.js
@@ -8,8 +8,6 @@ const main = document.getElementsByClassName("customize_order_main")[0];
 const type_error = document.getElementById("type_error");
 const toppings_error = document.getElementById("toppings_error");
 
-
-
 let counter = 599;
 setInterval(()=>{
   let time_formatted = String(Math.floor(counter / 60)).padStart(2, '0') + ":" + String((counter % 60)).padStart(2, '0');
@@ -24,27 +22,25 @@ setInterval(()=>{
 
 let base_price = 0.00;
 let topping_price = 0.00;
+
 function render_price() {
   const price = document.getElementById("total_price");
   price.textContent = "Total Price: $" + (base_price + topping_price).toFixed(2).toString();
 }
 
-function render_errors() {
-   
-}
-
 let form = main.getElementsByTagName("form")[0];
-let btns = main.getElementsByTagName("button");
+let btns = form.getElementsByTagName("button");
 
 btns[0].addEventListener("click", ()=>{
   if (!type_cup.checked && !type_cone.checked) {
-    toppings_error.innerHTML = <b>RAAA</b>;
-    render_errors();
+    type_error.innerHTML = "<b>Alert: Must choose a type</b>";
   }
   else {
+    type_error.innerHTML = "";
     base_price = 6.00
-    render_price();
   }
+  render_price();
+  render_errors();
 });
 
 btns[1].addEventListener("click", ()=>{
@@ -54,7 +50,28 @@ btns[1].addEventListener("click", ()=>{
       topping_price += 1.5;
     }
   }
+  if (topping_price == 0) {
+    toppings_error.innerHTML = "<b>Alert: Must choose at least one topping</b>";
+  }
+  else {
+    toppings_error.innerHTML = "";
+  }
   render_price();
+  render_errors();
+});
+
+btns[2].addEventListener("click", ()=>{
+  var is_error = false;
+  if (base_price == 0) {
+    type_error.innerHTML = "<b>Alert: Must choose a type</b>";
+    is_error = true;
+  }
+  if (topping_price == 0) {
+    toppings_error.innerHTML = "<b>Alert: Must choose at least one topping</b>";
+    is_error = true;
+  }
+  if (is_error) return;
+  window.location.href = "order_summary.html";
 });
 
 render_price();
